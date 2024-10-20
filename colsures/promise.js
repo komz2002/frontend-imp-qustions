99c9411710e83f9275a11288da129dbd1e584b9c
Komal Sharma <ks469828@gmail.com>
2024-10-19 23:00:41 -0400

    second commit

--- colsures/promise.js
+++ colsures/promise.js
@@ -28,25 +28,78 @@
 
 // 
 
+// const promiseThree=new Promise(function(resolve,reject){
+//   setTimeout(function(){
+//     resolve({username:"chai",email:"ks469828@gmail.com"})
 
-const promiseFour=new Promise(function(resolve, reject) {
-setTimeout(function(){
-  let error=true
-  if(!error){
-    resolve({username:"komal",passward:"12"})
+//   },1000)
 
-  }else{
-    reject('Error:Something went wrong')
-  }
+// })
+// promiseThree.then(function(user){
+//   console.log(user);
+
+// })
+
+
+
+
+// const promiseFour=new Promise(function(resolve,reject){
+//   setTimeout(function(){
+//     let error=false
+//     if(!error){
+//       resolve({usename:"komal",email:"122@gmail.com"})
+//     }
+//     else{
+//       reject("ERROR:something went wrong" )
+//     }
+
+
+// },1000)
+
+// })
+// promiseFour
+// .then((user)=>{
+//   console.log(user);
+//   return user.username
+
+// })
+// .then((username)=>{
+//   console.log(username);
+
+
+// })
+// .catch(function(error){
+//   console.log(error)
+
+// })
+// .finally(()=>console.log("the promise is either resoved or rejected"))
+
+
+
+//promise by using async and await
+
+const promisefive=new Promise(function(resolve,reject){
+  setTimeout(function(){
+    let error=true
+    if(!error){
+      resolve({username:"komal",email:"123@gmail.com"})
+    }
+    else{
+      reject("the: promise is rejected")
+    }
+
+  },1000)
+
+});
+
+async function consumePromiseFive(){
+  try{
+  const response=await promisefive
+  console.log(response);
+}catch(error){
+console.log(error);
+}
+}
+consumePromiseFive();
   
-
-
-},1000)
-})
-promiseFour .then().catch()
-
-
-
-
-
 
